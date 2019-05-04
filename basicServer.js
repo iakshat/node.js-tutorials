197cbd561875ea2f50c1b14324cf6f6f051f9d30
iakshat <akshat1118@gmail.com>
2019-05-04 21:33:08 +0530

    send json file to browser

--- basicServer.js
+++ basicServer.js
@@ -1,8 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("Hola!");
+	res.writeHead(200, {'Content-Type' : 'application/json'});
+
+	var jsonObj = {
+		name : 'akshat',
+		surname : 'shukla',
+		age : 18
+	};
+
+	res.end(JSON.stringify(jsonObj));
 
 }).listen(2000);
 
