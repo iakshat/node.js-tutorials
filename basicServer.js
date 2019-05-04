25fb1b6af7948d61525af321bab7f88b654f9904
iakshat <akshat1118@gmail.com>
2019-05-04 20:48:21 +0530

    send response to browser with pipe

--- basicServer.js
+++ basicServer.js
@@ -1,9 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer((req, res) => {
+//var writeStream = fs.createWriteStream('stream.txt');
+
+http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("Hola!");
+	
+	var readStream = fs.createReadStream('text.txt', 'utf8');
+	//IF THE STREAM IS TOO BIG THEN IT GOES FOR 
+	//DOWNLOAD IN FORM OF TXT FILE
+	readStream.pipe(res);
 
 }).listen(2000);
 
-console.log("Server started on http://127.0.0.1:8080");
+console.log("Server started on http://127.0.0.1:2000");
