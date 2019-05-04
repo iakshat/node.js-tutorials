c2104118d8cacaba48621807c189ac2737200b9a
iakshat <akshat1118@gmail.com>
2019-05-04 16:14:46 +0530

    read big file in chunks

--- basicServer.js
+++ basicServer.js
@@ -1,4 +1,20 @@
 var http = require('http');
+var fs = require('fs');
+
+var stream = fs.createReadStream('file.txt');
+stream.setEncoding('utf8');
+var data = '';
+
+stream.on('data', (str) => {
+	 console.log('-------------------------');
+	 console.log(str);
+	 data += str;
+});
+
+stream.on('end', () => {
+	console.log('----------End_----------');
+	//console.log(data);
+});
 
 var server = http.createServer((req, res) => {
 	res.end("Hola!");
