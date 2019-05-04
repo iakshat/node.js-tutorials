ac5015f18323fc4c15221fa57509eaf690da4880
iakshat <akshat1118@gmail.com>
2019-05-04 21:48:49 +0530

    send res as per URL recieved

--- basicServer.js
+++ basicServer.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("Hola!");
+
+	console.log('requested url is: ' +req.url);
+
+	if(req.url === '/' || req.url === '/home'){
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		fs.createReadStream('home.html').pipe(res);
+	}else{
+		res.writeHead(404);
+		res.end("404 Page Not FOund");
+	}
+	
+	// res.writeHead(200, {'Content-Type' : 'text/plain'});
+	// res.end("Hola!");
 
 }).listen(2000);
 
